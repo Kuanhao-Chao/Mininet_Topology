b8cf7e51adfe919872468efd4ba007fd1ddbcbc6
HowardChao <ntueeb05howard@gmail.com>
2018-03-28 01:47:51 +0800

    ls

--- new_topology.py
+++ new_topology.py
@@ -10,15 +10,15 @@ from mininet.link import TCLink, Intf
 from subprocess import call
 import csv
 
-with open('topology_data/connection_100_v2.csv', 'rb') as csvfile:
+with open('topology_data/try_20.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     for row in spamreader:
         print ', '.join(row)
 
 # def parse_file():
 line_list = []
-with open('topology_data/try_3.csv') as f:
-    for each_line in f: 
+with open('topology_data/try_20.csv') as f:
+    for each_line in f:
         if each_line[0] == '#':
             print(each_line)
             continue
@@ -31,6 +31,7 @@ with open('topology_data/try_3.csv') as f:
 bottom_start_id = int(line_list[0][0])
 switch_num = int(line_list[0][1])
 bandwidth = int(line_list[0][2])
+print(line_list)
 
 def runNet():
     "Create and run the network"
@@ -65,13 +66,12 @@ def runNet():
 #                 element_inner = h
 
     info( '*** Add links\n')
-    for i in line_list[switch_num+1:-1]:
+    for i in line_list[switch_num+1:]:
 #             info( "*** Adding links\n" )
 #             print(i[0])
-#             print(i[1])
         net.addLink(switch_node_list[int(i[0])], switch_node_list[int(i[1])])
         print("s"+str(int(i[0])), "s"+str(int(i[1])))
-        
+
     for i, element in enumerate(line_list[bottom_start_id+1:switch_num+1]):
         net.addLink(switch_node_list[int(element[0])], host_node_list[i])
         print("s"+element[0], "h"+ str(i+20))
@@ -83,13 +83,13 @@ def runNet():
         controller.start()
 
     info( '*** Starting switches\n')
-    for x in range(106):
+    for x in range(40):
         input_switch = 's' + str(x)
         net.get(input_switch).start([c0])
         print(input_switch, '  Starting')
-    
+
     info( '*** Post configure switches and hosts\n')
-    
+
     CLI(net)
     net.stop()
 
